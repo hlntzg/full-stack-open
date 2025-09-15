e86ed0597bcdfa76683debb8e2e7a0f2d6f8c67f
Helena Utzig <helenautzig@gmail.com>
2025-09-15 15:00:26 +0300

    [part01] ex01 - course information, step 1

--- part01/introdemo/src/App.jsx
+++ part01/introdemo/src/App.jsx
@@ -1,10 +1,38 @@
-const App = () => {
-  console.log('Hello from component')
+
+const Hello = (props) => {
+
+  console.log(props)
+
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   )
 }
 
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/hlntzg'>hlntzg</a> ♡
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'peter'
+  const age = 11
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello 
+        name='bob'
+        age={10}
+      />
+      <Hello name={name} age={age}/>
+      <Footer />
+    </>
+  )
+}
 export default App
