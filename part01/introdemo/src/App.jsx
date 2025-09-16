e5dede39ef0e3b3d2a5134c89f7ebab4778a4547
Helena Utzig <helenautzig@gmail.com>
2025-09-16 12:28:27 +0300

    [introdemo] complex state

--- part01/introdemo/src/App.jsx
+++ part01/introdemo/src/App.jsx
@@ -1,49 +1,40 @@
 import { useState } from "react"
 
-// const Display = ({ counter }) => {
-//   return (
-//     <div>{counter}</div>
-//   )
-// }
-const Display = ({ counter }) => <div>{counter}</div>
-
-// const Button = (props) => {
-//   return (
-//     <button onClick={props.onClick}>
-//       {props.text}
-//     </button>
-//   )
-// }
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-
 const App = () => {
-  const [ counter, setCounter] = useState(0)
-  // console.log('rendering with counter value', counter)
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
 
-  // handle increase
-  const increaseByOne = () => setCounter(counter + 1)
-  // handle descrease
-  const decreaseByOne = () => setCounter(counter - 1)
+  // const handleLeftClick = () => {
+  //   const newClicks = { 
+  //     ...clicks, // object spread syntax
+  //     left: clicks.left + 1 
+  //   }
+  //   setClicks(newClicks)
+  // }
 
-  // handle reset to zero
-  const setToZero = () => setCounter(0)
+  // const handleRightClick = () => {
+  //   const newClicks = { 
+  //     ...clicks, // object spread syntax
+  //     right: clicks.right + 1 
+  //   }
+  //   setClicks(newClicks)
+  // }
+
+  const handleLeftClick = () =>
+    setClicks({ ...clicks, left: clicks.left + 1 })
+
+  const handleRightClick = () =>
+    setClicks({ ...clicks, right: clicks.right + 1 })
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        onClick={increaseByOne} // event handler
-        text='plus'
-      />
-      <Button
-        onClick={setToZero} // event handler
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne} // event handler
-        text='minus'
-      />
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
     </div>
   )
 }
+
 export default App
