1b160afb8f882dfdc9ad99fd28a1ecfae526af27
Helena Utzig <helenautzig@gmail.com>
2025-09-16 11:57:58 +0300

    [introdemo] refactoring components, compacting arrow functions

--- part01/introdemo/src/App.jsx
+++ part01/introdemo/src/App.jsx
@@ -1,26 +1,30 @@
 import { useState } from "react"
 
-const Display = ({ counter }) => {
-  return (
-    <div>{counter}</div>
-  )
-}
+// const Display = ({ counter }) => {
+//   return (
+//     <div>{counter}</div>
+//   )
+// }
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
+// const Button = (props) => {
+//   return (
+//     <button onClick={props.onClick}>
+//       {props.text}
+//     </button>
+//   )
+// }
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
   const [ counter, setCounter] = useState(0)
+  // console.log('rendering with counter value', counter)
 
   // handle increase
   const increaseByOne = () => setCounter(counter + 1)
   // handle descrease
   const decreaseByOne = () => setCounter(counter - 1)
+
   // handle reset to zero
   const setToZero = () => setCounter(0)
 
@@ -28,15 +32,15 @@ const App = () => {
     <div>
       <Display counter={counter}/>
       <Button
-        onClick={increaseByOne}
+        onClick={increaseByOne} // event handler
         text='plus'
       />
       <Button
-        onClick={setToZero}
+        onClick={setToZero} // event handler
         text='zero'
       />     
       <Button
-        onClick={decreaseByOne}
+        onClick={decreaseByOne} // event handler
         text='minus'
       />
     </div>
