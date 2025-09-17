cd38feb268ea61946075b760d81c863cc68c4fc0
Helena Utzig <helenautzig@gmail.com>
2025-09-17 11:21:55 +0300

    [introdemo] Passing Event Handlers to Child Components

--- part01/introdemo/src/App.jsx
+++ part01/introdemo/src/App.jsx
@@ -1,64 +1,35 @@
 import { useState } from "react"
 
-// conditional rendering: component renders completely different 
-// React elements depending on the state of the application
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
-
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updatedLeft = left + 1
-    setLeft(updatedLeft)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'));
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-  }
-
-  const handleResetClick = () => {
-    setAll([])
-    setLeft(0)
-    setRight(0)
+  const setToValue = (newValue) => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
   return (
     <div>
-      {left}
-      <Button
-        onClick={handleLeftClick}
-        text='left'
+      {value}
+      <Button 
+        onClick={() => setToValue(1000)}
+        text='thousand'
       />
       <Button 
-        onClick={handleRightClick}
-        text='right'
+        onClick={() => setToValue(0)}
+        text='reset'
       />
-      {right}
-      <History allClicks={allClicks} />
-      <button onClick={handleResetClick}>Reset</button>
+      <Button
+        onClick={() => setToValue(value + 1)}
+        text='increment'
+      />
     </div>
   )
 }
-
 export default App
