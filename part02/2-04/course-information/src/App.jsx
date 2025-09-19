f8afbfba9a8965db19b8fe6d50f4493abf02f70d
Helena Utzig <helenautzig@gmail.com>
2025-09-19 12:19:01 +0300

    [part02] ex04 - course information, step 9

---
+++ part02/2-04/course-information/src/App.jsx
@@ -0,0 +1,103 @@
+
+const Course = (props) => {
+  return (
+    <div>
+      <Header course={props.course.name}/>
+      <Content parts={props.course.parts}/>
+      <Total parts={props.course.parts}/>
+    </div>
+  )
+} 
+
+const Header = (props) => {
+  return (
+    <div>
+      <h2>{props.course}</h2>
+    </div>
+  )
+}
+
+const Content = (props) => {
+
+  return (
+    <div>
+      {props.parts.map(part => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  )
+}
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => {
+  console.log(props)
+  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <div>
+      <h4>total of {total} exercises</h4>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {/* <Course course={courses} /> */}
+    </div>
+    )
+}
+
+export default App
