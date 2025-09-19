bf7fb356d1b82715914489835515f8d3297c126f
Helena Utzig <helenautzig@gmail.com>
2025-09-19 14:29:12 +0300

    [demo] controlled component

--- part02/demo/src/App.jsx
+++ part02/demo/src/App.jsx
@@ -1,23 +1,56 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
-  // const result = notes.map(notes => notes.id)
-  // console.log(result)
+// problem: const App = ({ notes }) => { ... } -> creates a self-reference error
+const App = (props) => {
+
+  // console.log(props.notes)
+
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
+
+  const addNote = (event) => {
+    event.preventDefault()
+
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+    setNotes(notes.concat(noteObject))
+
+    // resets the value of the controlled input element 
+    // by calling the setNewNote function of the newNote state
+    setNewNote('a new note...')
+  }
+  
+  // event handler that synchronizes the changes made to the input
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map(note => 
-          // <li key={note.id}>
-          //   {note.content}
-          // </li>
           <Note 
             key={note.id}
             note={note} 
           />
         )}
       </ul>
+      <form 
+        onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">
+            save
+        </button>
+      </form> 
     </div>
   )
 }
