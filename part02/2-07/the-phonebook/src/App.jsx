e707635c3e39561f0e58a405f40c4a2757d4eb52
Helena Utzig <helenautzig@gmail.com>
2025-09-19 16:38:36 +0300

    [part02] ex07 - the phonebook, step 2

---
+++ part02/2-07/the-phonebook/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([{name: 'Arto Hellas'}]) 
+  const [newName, setNewName] = useState('input your name here')
+
+  const handleNewName = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const invalidName = persons.find(person => person.name === newName)
+    if (invalidName){
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const personObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('input your name here')
+  }
+
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: 
+          <input
+            value={newName}
+            onChange={handleNewName}
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            onClick={addPerson}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+
+      {persons.map(person =>          
+        <p key={person.name}>
+          {person.name}
+        </p>
+      )}
+    </div>
+  )
+}
+
+export default App
