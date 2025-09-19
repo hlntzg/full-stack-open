ac9f6b3f3d3c1c0a3bff3198a6a81a5a2b72932c
Helena Utzig <helenautzig@gmail.com>
2025-09-19 10:05:04 +0300

    [demo] refactoring modules, import components

--- part02/demo/src/App.jsx
+++ part02/demo/src/App.jsx
@@ -1,22 +1,21 @@
-const App = (props) => {
-  const { notes } = props
+import Note from './components/Note'
 
-  const result = notes.map(notes => notes.content)
-  console.log(result)
+const App = ({ notes }) => {
+  // const result = notes.map(notes => notes.id)
+  // console.log(result)
 
-  // map method is used to create view elements, 
-  // the value of the variable must be rendered inside curly braces
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {/* <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li> */}
         {notes.map(note => 
-          <li key={note.id}>
-            {note.content}
-          </li>
+          // <li key={note.id}>
+          //   {note.content}
+          // </li>
+          <Note 
+            key={note.id}
+            note={note} 
+          />
         )}
       </ul>
     </div>
