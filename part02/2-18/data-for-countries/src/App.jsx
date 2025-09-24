81fe353a6208039ec91fe98b34db37cb94bc88f8
Helena Utzig <helenautzig@gmail.com>
2025-09-24 16:06:38 +0300

    [part02] ex17 - data for countries, step 1

---
+++ part02/2-18/data-for-countries/src/App.jsx
@@ -0,0 +1,70 @@
+
+import { useEffect, useState } from 'react'
+import countriesService from './services/countries'
+import SearchCountry from './components/SearchCountry'
+import CountriesList from './components/CountriesList'
+import DisplayCountry from './components/DisplayCountry'
+
+const App = () => {
+  const [countries, setCountries] = useState(null)
+  const [searchCountry, setSearchCountry] = useState('')
+  const [selectedCountry, setSelectedCountry] = useState(null)
+
+  useEffect(() => {
+    //  GET request 
+    countriesService
+      .getAll()
+      .then(initialCountries => {
+        setCountries(initialCountries)
+        console.log(initialCountries)
+      })
+  }, [])
+  
+  if (!countries) { 
+    return null 
+  }
+  
+  const handleCountryChange = (event) => {
+    const value = event.target.value
+    setSearchCountry(value)
+
+    if (value) {
+      const matches = countries.filter(country =>
+        country.name.common.toLowerCase().includes(value.toLowerCase())
+      )
+
+      if (matches.length === 1) {
+        //  GET request for selected country
+        countriesService
+          .getByCountry(matches[0].name.common)
+          .then(countryData => {
+            setSelectedCountry(countryData)
+          })
+      } else {
+        setSelectedCountry(null)
+      }
+    } else {
+      setSelectedCountry(null)
+    }
+  }
+
+  const filteredCountries = countries.filter(country =>
+    country.name.common.toLowerCase().includes(searchCountry.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h1>Data for Countries</h1>
+      <SearchCountry searchCountry={searchCountry} handleSearchCountry={handleCountryChange} />
+      {filteredCountries.length > 10 && <p>Too many matches, specify another filter</p>}
+      {filteredCountries.length > 1 && filteredCountries.length <= 10 && (
+        <CountriesList countries={filteredCountries} />
+      )}
+      {filteredCountries.length === 1 && (
+        <DisplayCountry country={selectedCountry} />
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ part02/2-18/data-for-countries/src/components/DisplayCountry.jsx
@@ -0,0 +1,21 @@
+const DisplayCountry = ({ country }) => {
+  if (!country) return null
+
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <div>Capital: {country.capital}</div>
+      <div>Area: {country.area}</div>
+      <h2>Languages</h2>
+      <ul>
+        {Object.values(country.languages).map(language => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img 
+        src={country.flags.png} alt={`Flag of ${country.name.common}`}  
+      />
+    </div>
+  )
+}
+export default DisplayCountry
