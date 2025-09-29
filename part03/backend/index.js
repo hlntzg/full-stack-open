7d6bd7f30bbce0db8ecae67455189265d29c3843
Helena Utzig <helenautzig@gmail.com>
2025-09-29 11:55:32 +0300

    [part03] REST - deleting a resource

--- part03/backend/index.js
+++ part03/backend/index.js
@@ -41,6 +41,14 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
+// route for deleting a resource
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
