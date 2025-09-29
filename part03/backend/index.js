bb0a06ee245d6452c8f2ec1193e6168adf80bc75
Helena Utzig <helenautzig@gmail.com>
2025-09-29 12:34:18 +0300

    [part03] REST - WebStorm HTTP client; receiving data

--- part03/backend/index.js
+++ part03/backend/index.js
@@ -49,6 +49,41 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
+// route for creating a new resource
+app.use(express.json()) // middleware for parsing JSON bodies
+// Without the json-parser, the body property would be undefined. 
+// The json-parser takes the JSON data of a request, transforms it into
+// a JavaScript object and then attaches it to the body property of 
+// the request object before the route handler is called.
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    // get the maximum id in the notes array
+    // note to spread operator (...) which expands the array into a list of values
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+  response.json(note)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
