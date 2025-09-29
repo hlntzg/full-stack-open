06ed19a0040ae5b806eeb5b89cc0cd8158aa1a2c
Helena Utzig <helenautzig@gmail.com>
2025-09-29 10:44:29 +0300

    [part03] automatic change tracking -- watch option

--- part03/backend/index.js
+++ part03/backend/index.js
@@ -1,14 +1,36 @@
-// imports Node's built-in web server module
-const http = require('http')
+// import express which this time is a function used to create
+// an Express application and stored in the app variable
+const express = require('express')
+const app = express()
 
-// 1 - createServer method of the http module to create a new web server
-// 2 - every time an HTTP request is made to the server, the provided callback function is executed
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.get('/', (request, response) => {
+  // send method of the response object is used to send the response
+  response.send('<h1>Hello World!</h1>')
 })
 
-// 3 - bind the http server assigned to the app variable to listen on port 3001 for incoming requests
+app.get('/api/notes', (request, response) => {
+  // the json method of the response object can be used to send a JSON response
+  response.json(notes)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
