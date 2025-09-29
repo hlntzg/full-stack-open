a03270a473f4afa19b30f2b0060c3a5ece948611
Helena Utzig <helenautzig@gmail.com>
2025-09-29 13:52:14 +0300

    [part03] ex03 - phonebook backend, step 3

---
+++ part03/3-03/index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423124"
+    }
+]
+
+// route for fetching all resources
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+// route for fetching phonebook info and current date
+app.get('/info', (request, response) => {
+  const date = new Date()
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>
+  `)
+})
+
+// route for fetching a single person by id
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+// bind the application to port 3001
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
