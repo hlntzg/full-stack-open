418bdacec2b41f6c0896cf02220a14ceee6b8ddd
Helena Utzig <helenautzig@gmail.com>
2025-09-30 11:56:34 +0300

    [notes] rm cors from backend/index.js

--- part03/render/backend/index.js
+++ part03/render/backend/index.js
@@ -31,7 +31,7 @@ const requestLogger = (request, response, next) => {
 
 // const cors = require('cors')
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 app.use(express.static('dist'))
