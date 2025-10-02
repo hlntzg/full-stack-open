a96ab5154055704a88a5e2cdb51e32b9222ca23a
Helena Utzig <helenautzig@gmail.com>
2025-10-02 11:41:06 +0300

    [part03] ex10 - phonebook backend main page welcome, step 10

--- part03/3-11/backend/index.js
+++ part03/3-11/backend/index.js
@@ -52,6 +52,10 @@ app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 
+app.get('/', (req, res) => {
+  res.send('<h1>Phonebook backend</h1>')
+})
+
 // route for fetching all resources
 app.get('/api/persons', (request, response) => {
   response.json(persons)
