a9b5e1d476d517f61eccef6eab7bdda8bf68fd7d
Helena Utzig <helenautzig@gmail.com>
2025-10-03 13:47:00 +0300

    [notes] dotenv config

--- part03/render/backend/index.js
+++ part03/render/backend/index.js
@@ -73,11 +73,11 @@ app.get('/api/notes/:id', (request, response) => {
 })
 
 // route for deleting a resource
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+// Mongoose's findById method
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
 const generateId = () => {
@@ -94,19 +94,17 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 // handler of requests made to unknown endpoints
