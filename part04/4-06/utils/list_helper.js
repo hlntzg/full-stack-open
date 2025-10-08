17e81b535b759d75481ec638346b8fb959ced8e3
Helena Utzig <helenautzig@gmail.com>
2025-10-08 15:06:33 +0300

    [part04] ex06 - blog list, Helper Functions and Unit Tests, step 4

--- part04/4-06/utils/list_helper.js
+++ part04/4-06/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -12,7 +14,7 @@ const totalLikes = (blogs) => {
     : blogs.reduce(reducer, 0)
 }
 
-// return an object with the title, author, and likes fields
+// returns an object with the title, author, and likes fields
 const mostLiked = (blogs) => {
   if (blogs.length === 0) {
     return {}
@@ -29,9 +31,34 @@ const mostLiked = (blogs) => {
     likes: favorite.likes,
   }
 }
+// const mostLiked = (blogs) => {
+//   const favorite = _.maxBy(blogs, 'likes')
+//   return favorite
+//     ? { title: favorite.title, author: favorite.author, likes: favorite.likes }
+//     : {}
+// }
+
+
+// returns the author who has the largest amount of blogs and
+// the total number of blogs that the top author has
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return {}
+  }
+
+  const counts = _.countBy(blogs, 'author')
+  const pairs = _.toPairs(counts)
+  const topAuthor = _.maxBy(pairs, ([, count]) => count)
+
+  return {
+    author: topAuthor[0],
+    blogs: topAuthor[1],
+  }
+}
 
 module.exports = {
     dummy,
     totalLikes,
-    mostLiked
+    mostLiked,
+    mostBlogs,
 }
