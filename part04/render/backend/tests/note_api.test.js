4c392e611774ae0bc4e86748159ab3dfbb38a947
Helena Utzig <helenautzig@gmail.com>
2025-10-15 16:01:07 +0300

    [notes] Initializing the database before tests

--- part04/render/backend/tests/note_api.test.js
+++ part04/render/backend/tests/note_api.test.js
@@ -1,13 +1,34 @@
 // supertest only use the Express application defined in the app.js file
 // supertest provides functions like expect()
 
-const { test, after } = require('node:test')
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const assert = require('node:assert')
 
 const api = supertest(app)
+
+// reset the database and generate the needed test data in a controlled manner
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false,
+  },
+  {
+    content: 'Browser can execute only JavaScript',
+    important: true,
+  },
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
+
 
 test('notes are returned as json', async () => {
   await api
@@ -19,14 +40,13 @@ test('notes are returned as json', async () => {
 test('all notes are returned', async () => {
   const response = await api.get('/api/notes')
 
-  assert.strictEqual(response.body.length, 2)
+  assert.strictEqual(response.body.length, initialNotes.length)
 })
 
 test('a specific note is within the returned notes', async () => {
   const response = await api.get('/api/notes')
 
   const contents = response.body.map(e => e.content)
-//   assert.strictEqual(contents.includes('HTML is easy'), true)
   assert(contents.includes('HTML is easy'))
 })
 
