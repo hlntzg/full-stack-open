b4c79542b0a061ba9105a4e4d0a0364af03e440a
Helena Utzig <helenautzig@gmail.com>
2025-12-02 15:17:12 +0200

    [part04] ex20 - Blog List Expansions, step 8

---
+++ part04/4-20/controllers/blogs.js
@@ -0,0 +1,87 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+// user authentication
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization')
+//   if (authorization && authorization.startsWith('Bearer ')) {
+//     return authorization.replace('Bearer ', '')
+//   }
+//   return null
+// }
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs)
+})
+
+blogsRouter.post('/', async (request, response, next) => {    
+  try {
+    const body = request.body
+    
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+      }
+    const user = await User.findById(decodedToken.id)
+
+    if (!user) {
+      return response.status(400).json({ error: 'userId missing or not valid' })
+    }
+
+    // VALIDATION
+    if (!body.title || !body.url) {
+      return response.status(400).json({ error: 'title or url missing' })
+    }
+
+    const blog = new Blog ({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id
+    })
+
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
+    response.status(201).json(savedBlog)
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.delete('/:id', async(request, response, next) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.put('/:id', async(request, response, next) => {
+  try {
+    const body = request.body
+
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      request.params.id,
+      { likes: body.likes },
+      { new: true, runValidators: true }
+    )
+
+    if (!updatedBlog) {
+      return response.status(404).end()
+    }
+
+    response.json(updatedBlog)
+
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = blogsRouter
