db693b073847e7f4a19af255cfee4e9cffcfc268
Helena Utzig <helenautzig@gmail.com>
2025-12-05 16:49:27 +0200

    [notes] part05 - Frontend, Clicking buttons in tests

--- part05/render/frontend/src/components/Note.test.jsx
+++ part05/render/frontend/src/components/Note.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event' // clicking buttons tests
 import Note from './Note'
 
 test('renders content', async () => {
@@ -38,24 +39,21 @@ test('renders content: does not render this', () => {
   expect(element).toBeNull()
 })
 
-
-// It is, however, recommended to search for elements primarily using methods other than
-// the container object and CSS selectors. CSS attributes can often be changed without
-// affecting the application's functionality, and users are not aware of them. It is better
-// to search for elements based on properties visible to the user, for example, by using the
-// getByText method. This way, the tests better simulate the actual nature of the component
-// and how a user would find the element on the screen.
-test('renders content: querySelector of the object Container', () => {
+test('clicking the button calls event handler once', async () => {
   const note = {
     content: 'Component testing is done with react-testing-library',
     important: true
   }
+  
+  const mockHandler = vi.fn()
 
-  const { container } = render(<Note note={note} />)
-
-  const div = container.querySelector('.note')
-  //   console.log(container)
-  expect(div).toHaveTextContent(
-    'Component testing is done with react-testing-library'
+  render(
+    <Note note={note} toggleImportance={mockHandler} />
   )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
