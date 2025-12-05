4c5cb11b2040555f4ce05da6612f9490861abc3c
Helena Utzig <helenautzig@gmail.com>
2025-12-05 12:26:05 +0200

    [part05] ex10 - Blog List Frontend, step 10

---
+++ part05/5-10/bloglist-frontend/src/components/Blog.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import blogService from '../services/blogs'
+
+const Blog = ({ blog, updateBlog }) => {
+
+  // console.log(blog)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const hideWhenVisible = { display: detailsVisible ? 'none' : '' }
+  const showWhenVisible = { display: detailsVisible ? '' : 'none' }
+
+  const addLike = () => {
+    console.log('clicked')
+    const updatedBlog = {
+      ...blog,
+      likes: blog.likes + 1
+    }
+
+    updateBlog(blog.id, updatedBlog)
+    console.log(blog)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        <div>
+          {blog.title}
+          <button onClick={() => setDetailsVisible(true)}>view</button>
+        </div>
+      </div>
+      <div style={showWhenVisible}>
+        <div>
+          {blog.title}
+          <button onClick={() => setDetailsVisible(false)}>hide</button>
+        </div>
+        <div>
+          {blog.author}
+        </div>
+        <div>
+          {blog.likes}
+          <button onClick={addLike}>like</button>
+        </div>
+        <div>
+          {blog.user?.name || "unknown"}
+        </div>
+    </div>
+    </div> 
+  )
+}
+
+export default Blog
