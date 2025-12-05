a829e01a1eb9ce7a55100b53d9207fe6375cf983
Helena Utzig <helenautzig@gmail.com>
2025-12-05 10:17:41 +0200

    [part05] ex05 - Blog List Frontend, step 5

---
+++ part05/5-05/bloglist-frontend/src/components/BlogForm.jsx
@@ -0,0 +1,102 @@
+import { useState } from 'react'
+
+const BlogForm = ({ createBlog }) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    
+    createBlog({
+      title: title,
+      author: author,
+      url: url,
+    })
+  // reset the form fields
+  setTitle('')
+  setAuthor('')
+  setUrl('')
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+
+      <form onSubmit={addBlog}>
+        <div>
+          title:
+          <input
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+          />
+        </div>
+
+        <div>
+          author:
+          <input
+            value={author}
+            onChange={event => setAuthor(event.target.value)}
+          />
+        </div>
+
+        <div>
+          url:
+          <input
+            value={url}
+            onChange={event => setUrl(event.target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default BlogForm
+
+// const BlogForm = ({
+//   onSubmit,
+//   title,
+//   author,
+//   url,
+//   handleTitleChange,
+//   handleAuthorChange,
+//   handleUrlChange
+// }) => {
+//   return (
+//     <div>
+//       <h2>create new</h2>
+
+//       <form onSubmit={onSubmit}>
+//         <div>
+//           title:
+//           <input
+//             value={title}
+//             onChange={handleTitleChange}
+//           />
+//         </div>
+
+//         <div>
+//           author:
+//           <input
+//             value={author}
+//             onChange={handleAuthorChange}
+//           />
+//         </div>
+
+//         <div>
+//           url:
+//           <input
+//             value={url}
+//             onChange={handleUrlChange}
+//           />
+//         </div>
+
+//         <button type="submit">create</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default BlogForm
