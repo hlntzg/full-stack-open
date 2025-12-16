5c6202a9b8540cd19765d190411fcf83f82d3e8a
Helena Utzig <helenautzig@gmail.com>
2025-12-16 10:57:33 +0200

    [notes] part05 - Frontend, Tests for the Togglable component, Testing the forms

---
+++ part05/render/frontend/src/components/NoteForm.test.jsx
@@ -0,0 +1,26 @@
+// simulate text input with userEvent
+
+import { render, screen } from '@testing-library/react'
+import NoteForm from './NoteForm'
+import userEvent from '@testing-library/user-event'
+
+test('<NoteForm /> updates parent state and calls onSubmit', async () => {
+  const createNote = vi.fn()
+  const user = userEvent.setup()
+
+  render(<NoteForm createNote={createNote} />)
+
+  const input = screen.getByRole('textbox')
+  const sendButton = screen.getByText('save')
+
+  // the method type of the userEvent is used to write text to the input field
+  await user.type(input, 'testing a form...')
+  await user.click(sendButton)
+
+  console.log(createNote.mock.calls)
+  
+  expect(createNote.mock.calls).toHaveLength(1)
+  expect(createNote.mock.calls[0][0].content).toBe('testing a form...')
+
+
+})
