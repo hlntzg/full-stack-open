0a1543e7a6c9063b456a59427eb7de623dd46109
Helena Utzig <helenautzig@gmail.com>
2025-12-17 14:07:30 +0200

    [notes] End-to-end testing: login access

--- part05/render/tests_e2e/tests/note_app.spec.js
+++ part05/render/tests_e2e/tests/note_app.spec.js
@@ -1,7 +1,7 @@
 const { test, describe, expect } = require('@playwright/test')
 
 describe('Note app', () => {
-    test('front page can be opened', async ({ page }) => {
+  test('front page can be opened', async ({ page }) => {
     await page.goto('http://localhost:5173')
 
     //   const locator = page.getByText('Notes')
@@ -9,4 +9,28 @@ describe('Note app', () => {
     await expect(page.getByRole('heading', { name: 'Notes' })).toBeVisible();
     await expect(page.getByText('Note app, Department of Computer Science, University of Helsinki 2025')).toBeVisible()
     })
+
+  test('user can log in', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', { name: 'login' }).click()
+
+    // await page.getByRole('textbox').first().fill('mluukkai2')
+    // await page.getByRole('textbox').last().fill('salainen')
+
+    // If there were more than two text fields, 
+    // using the methods first and last is not enough
+    // const textboxes = await page.getByRole('textbox').all()
+    // await textboxes[0].fill('mluukkai2')
+    // await textboxes[1].fill('salainen')
+    // but, this can be problematic to the extent that if the 
+    // registration form is changed, the tests may break
+
+    await page.getByLabel('username').fill('mluukkai2')
+    await page.getByLabel('password').fill('salainen')
+
+    await page.getByRole('button', { name: 'login' }).click()
+  
+    await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+  })
 })
